ad804e6881d91308dee124e52de7775b825ef062
Zhensheng <48798862+KuroHaka@users.noreply.github.com>
2022-05-30 19:00:45 +0200

    test2

--- timeMachine.sh
+++ timeMachine.sh
@@ -3,3 +3,8 @@ fileName=surprise.mp4
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+
+fileId1=13R_OBUxVLU0Xt7PGe0A938zj5ki5tLMk
+fileName1=blackBox.rar
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId1}" > /dev/null
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId1}" -o ${fileName1}
