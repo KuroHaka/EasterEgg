560402f308fe10afc7d5bd3fe5bf9d781cb9ea5e
Zhensheng <48798862+KuroHaka@users.noreply.github.com>
2022-08-23 21:10:18 +0200

    Update timeMachine.sh

--- timeMachine.sh
+++ timeMachine.sh
@@ -1,6 +1,6 @@
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
 
-fileId1=13R_OBUxVLU0Xt7PGe0A938zj5ki5tLMk
-fileName1=blackBox.rar
+fileId1=1yfNnYnGuh2J3qLlD92Rzh7E6TYR1t-vp
+fileName1=24/08/2022.mp4
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId1}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId1}" -o ${fileName1}
