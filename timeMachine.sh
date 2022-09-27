bd373d90174c1f51dc2e834bd51db7e469ce37c0
Zhensheng <48798862+KuroHaka@users.noreply.github.com>
2022-09-27 17:48:56 +0200

    Update timeMachine.sh
    Yes

--- timeMachine.sh
+++ timeMachine.sh
@@ -1,5 +1,9 @@
 fileId=146GSgkvTHjpIZ312qkrLK582eCt9H0ey
 fileName=august.mp4
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+fileId2=1LU-WmfDzau4d99ahoW7rvWP8HuAMrpFK
+fileName2=september.mp4
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId2}" > /dev/null
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId2}" -o ${fileName2}
