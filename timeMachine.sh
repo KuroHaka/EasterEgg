3d6ba095842bf37080fcf158bf68149f481a250a
Zhensheng <48798862+KuroHaka@users.noreply.github.com>
2022-09-27 19:11:11 +0200

    Update timeMachine.sh

--- timeMachine.sh
+++ timeMachine.sh
@@ -1,9 +1,5 @@
-fileId=146GSgkvTHjpIZ312qkrLK582eCt9H0ey
-fileName=august.mp4
-fileId2=1LU-WmfDzau4d99ahoW7rvWP8HuAMrpFK
-fileName2=september.mp4
+fileId=1IaGoPuVPFsnx9_nHP-N9_1IqBc-cQZEt
+fileName=september.mp4
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId2}" > /dev/null
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId2}" -o ${fileName2}
