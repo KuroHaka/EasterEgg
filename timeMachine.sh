420db73c906d387a18f080dea83f939be0053238
Zhensheng <48798862+KuroHaka@users.noreply.github.com>
2022-10-12 21:52:23 +0200

    Update timeMachine.sh

--- timeMachine.sh
+++ timeMachine.sh
@@ -1,6 +1,7 @@
 fileName=september.mp4
 curl "https://firebasestorage.googleapis.com/v0/b/recyclapp-adbde.appspot.com/o/september.mp4?alt=media&token=fea0b4d0-083a-4d92-b31a-6d86646fcb12" -o ${fileName}
-
+fileName=october.mp4
+curl "https://firebasestorage.googleapis.com/v0/b/recyclapp-adbde.appspot.com/o/october.mp4?alt=media&token=cdd98cbf-8d69-4c43-9805-dd76b6107d74" -o ${fileName}
 fileId=146GSgkvTHjpIZ312qkrLK582eCt9H0ey
 fileName=august.mp4
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
